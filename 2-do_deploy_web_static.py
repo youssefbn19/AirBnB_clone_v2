dd16a9038a16f3dd347b149fa3cb6157ac3af558
Youssef Benchiir <ybenchiir@gmail.com>
2023-10-06 01:41:11 +0100

    Update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python3
 """ This module contains do_deloy function
 """
-from fabric.api import put, run, env
+from fabric.api import put, run, env, local
 from os import path
+from datetime import datetime
 
 env.hosts = ["34.239.255.45", "52.91.182.206"]
+
+
+def do_pack():
+    """
+      Generates a .tgz archive from the contents of the web_static
+      folder of your AirBnB Clone repo
+      Returns:
+          the archive path if the archive has been correctly generated.
+          Otherwise, it should return None
+    """
+    d = datetime.now()
+    file_name = f"web_static_{d.year}{d.month}{d.day}"\
+                f"{d.hour}{d.minute}{d.second}.tgz"
+    local("mkdir -p versions")
+    res = local(f"tar -cvzf versions/{file_name} web_static")
+    if res.succeeded:
+        return f"versions/{file_name}"
+    else:
+        return None
 
 
 def do_deploy(archive_path):
