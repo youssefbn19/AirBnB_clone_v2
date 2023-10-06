d3394434cab032e2fef451a6723e09ecfef33736
Youssef Benchiir <ybenchiir@gmail.com>
2023-10-06 02:09:10 +0100

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,7 +2,6 @@
 """ This module contains do_deloy function
 """
 from fabric.api import put, run, env, local
-from os import path
 from datetime import datetime
 
 env.hosts = ["34.239.255.45", "52.91.182.206"]
@@ -33,34 +32,19 @@ def do_deploy(archive_path):
       Returns:
           False if the file at the path archive_path doesn’t exist.
     """
-    if not path.exists(archive_path):
-        return False
-    res = put(archive_path, "/tmp/")
-    if res.failed:
-        return False
-    file_only = archive_path.split("/")[-1]
-    file = file_only.split(".")[0]
-    res = run(f"mkdir -p /data/web_static/releases/{file}")
-    if res.failed:
-        return False
-    res = run(f"tar -xzf /tmp/{file}.tgz -C /data/web_static/releases/{file}/")
-    if res.failed:
-        return False
-    res = run(f"rm /tmp/{file}.tgz")
-    if res.failed:
-        return False
-    res = run(f"rm -rf /data/web_static/current")
-    if res.failed:
-        return False
-    res = run(f"mv /data/web_static/releases/{file}/web_static/* "
+    try:
+        put(archive_path, "/tmp/")
+        file_only = archive_path.split("/")[-1]
+        file = file_only.split(".")[0]
+        run(f"mkdir -p /data/web_static/releases/{file}")
+        run(f"tar -xzf /tmp/{file}.tgz -C /data/web_static/releases/{file}/")
+        run(f"rm /tmp/{file}.tgz")
+        run(f"rm -rf /data/web_static/current")
+        run(f"mv /data/web_static/releases/{file}/web_static/* "
             f"/data/web_static/releases/{file}")
-    if res.failed:
+        run(f"rm -rf /data/web_static/releases/{file}/web_static")
+        run(f"ln -s /data/web_static/releases/{file} /data/web_static/current")
+        print("New version deployed!")
+        return True
+    except Exception:
         return False
-    res = run(f"rm -rf /data/web_static/releases/{file}/web_static")
-    if res.failed:
-        return False
-    res = run(f"ln -s /data/web_static/releases/{file} /data/web_static/current")
-    if res.failed:
-        return False
-    print("New version deployed!")
-    return True
