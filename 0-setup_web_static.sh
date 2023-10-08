caef915a6e4da4c5c7c91621115e1ccd927b8a00
Youssef Benchiir <ybenchiir@gmail.com>
2023-10-08 14:45:50 +0100

    Update last one

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-# install nginx and configure it to serve some content
-if ! which nginx > /dev/null 2>&1; then
-sudo apt-get -y update
-sudo apt-get -y install nginx
+# Sets up your web servers for the deployment of web_static.
+
+if (! dpkg -l | grep -q "nginx");then
+    sudo apt-get -y update
+    sudo apt-get install -y nginx
 fi
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo echo -e "<html>\n  <head>\n  </head>\n  <body>\n    ALX School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-myc="\n\tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/\;\n\t}\n"
-st="server {"
-sudo sed -i "s/^$st/$st$myc/" /etc/nginx/sites-enabled/default
 sudo service nginx start
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
+echo -e '<h1>Hello world!</h1>' | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R "ubuntu":"ubuntu" /data/
+sudo sed -i 's#^}$#\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n&#' /etc/nginx/sites-enabled/default
+sudo service nginx restart
