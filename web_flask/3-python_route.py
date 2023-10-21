7ce8bfb24584d5bf9f9674c0e129c2c250242ed1
Youssef Benchiir <ybenchiir@gmail.com>
2023-10-21 11:47:54 +0100

    Add new web app module

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+    This module starts a Flask web application run on
+    a global server in port 5000
+"""
+from cgitb import text
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """
+        Display a content when user navigate to the home page
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+        Display a content when user navigate to hbnb
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """
+        Display a content when user navigate to c followed by
+        the value of the text variable
+    """
+    txt = escape(text).replace('_', ' ')
+    return f'C {txt}'
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_text(text="is cool"):
+
+    """ Display a content when user navigate to python followed by
+        the value of the text variable.
+
+    Returns:
+        string: Python concatenate with a text pass as an argument
+    """
+    txt = escape(text).replace('_', ' ')
+    return f'Python {txt}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
