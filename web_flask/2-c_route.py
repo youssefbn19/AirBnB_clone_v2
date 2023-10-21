252100fba49a5f1c3bba2d8f7ae0db2f8a4f3663
Youssef Benchiir <ybenchiir@gmail.com>
2023-10-21 11:00:07 +0100

    Add new web app module

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+    This module starts a Flask web application run on
+    a global server in port 5000
+"""
+from cgitb import text
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """
+        Display a content when user navigate to the home page
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+        Display a content when user navigate to hbnb
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """
+        Display a content when user navigate to c followed by
+        the value of the text variable
+    """
+    txt = escape(text).replace('_', ' ')
+    return f'C {txt}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
